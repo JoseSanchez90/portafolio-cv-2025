a369743b06c16fd363e7f3546a1a7950fcd1ef32
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-02-20 09:44:17 -0500

    add python

--- app/typescript/skills.ts
+++ app/typescript/skills.ts
@@ -1,11 +1,12 @@
 export const skills = [
     { name: "Next.js", icon: "/nextjs.svg" },
     { name: "React.js", icon: "/react.svg" },
+    { name: "Vue.js", icon: "/vue.svg" },
     { name: "JavaScript", icon: "/javascript.svg" },
     { name: "TypeScript", icon: "/typescript.svg" },
+    { name: "Python", icon: "/python.svg" },
     { name: "Tailwind CSS", icon: "/tailwindcss.svg" },
     { name: "Firebase", icon: "/firebase.svg" },
-    { name: "Vue.js", icon: "/vue.svg" },
     { name: "Git", icon: "/git.svg" },
     { name: "MySQL", icon: "/mysql.svg" },
   ]
