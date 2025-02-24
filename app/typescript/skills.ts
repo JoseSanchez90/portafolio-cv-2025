64b6df0a527501d375a56d5a27e20272714a7824
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-02-23 19:57:07 -0500

    update skills, add c# & android

--- app/typescript/skills.ts
+++ app/typescript/skills.ts
@@ -2,6 +2,8 @@ export const skills = [
     { name: "Next.js", icon: "/nextjs.svg" },
     { name: "React.js", icon: "/react.svg" },
     { name: "Vue.js", icon: "/vue.svg" },
+    { name: "C#", icon: "/c-sharp.svg" },
+    { name: "Android Studio", icon: "/androidstudio.svg" },
     { name: "Python", icon: "/python.svg" },
     { name: "Flask", icon: "/flask.svg" },
     { name: "JavaScript", icon: "/javascript.svg" },
